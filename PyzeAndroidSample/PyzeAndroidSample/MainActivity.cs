ba7ac833e868faf0e91b427ec727c022b2bb800d
Jess <jess.loboz@gmail.com>
2017-01-16 11:10:33 +0530

    Committed initial sample projects

---
+++ PyzeAndroidSample/PyzeAndroidSample/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using pyze.xamarin.android;
+using pyze.xamarin.android.PyzeEvents;
+
+namespace PyzeAndroidSample
+{
+	[Activity(Label = "PyzeAndroidSample", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			Pyze.Initialize(this);
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button>(Resource.Id.myButton);
+
+			button.Click += delegate { 
+				button.Text = string.Format("{0} clicks!", count++);
+				PyzeCustomEvents.PostCustomEvent("Button Clicked!");
+				Pyze.ShowInAppMessage(this, (sender, e) =>
+				{
+					//Handle button click here	
+				});
+			};
+		}
+	}
+}
+
---
+++ PyzeiOSSample/PyzeiOSSample/Main.cs
@@ -0,0 +1,24 @@
+﻿using Foundation;
+using pyze.xamarin.iOS;
+using UIKit;
+
+namespace PyzeiOSSample
+{
+	public class Application
+	{
+		// This is the main entry point of the application.
+		static void Main(string[] args)
+		{
+			// if you want to use a different Application Delegate class from "AppDelegate"
+			// you can specify it here.
+			UIApplication.Main(args, null, "AppDelegate");
+
+			var keys = new object[] { "key1", "key2" };
+			var values = new object[] { "value1", "value2" };
+			var customAttributes = NSDictionary.FromObjectsAndKeys(values, keys);
+			PyzeAd.PostAdRequested("Google", "home", "(320, 160)", "banner", customAttributes);
+
+			PyzeCustomEvent.PostWithEventName("Blog Read");
+		}
+	}
+}
